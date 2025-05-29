9a7589833d9b1b0b2f228c5c98082ad16960bbd6
Stephen Shamaiengar <s.shamaiengar@gmail.com>
2025-05-28 23:16:56 -0700

    Update footer copyright to 2025

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -86,7 +86,7 @@ const DefaultLayout = ({ data, children, bodyClass, isHome }) => {
                     <footer className="site-foot">
                         <div className="site-foot-nav container">
                             <div className="site-foot-nav-left">
-                                <Link to="/">{site.title}</Link> © 2024
+                                <Link to="/">{site.title}</Link> © 2025
                             </div>
                             <div className="site-foot-nav-right">
                                 {/* <Navigation data={site.navigation} navClass="site-foot-nav-item" /> */}
